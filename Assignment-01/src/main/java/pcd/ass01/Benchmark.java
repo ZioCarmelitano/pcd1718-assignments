9786bc091ae7ed899506121a4351e058fc5cfb16
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-03-24 21:28:50 +0100

    Benchmark now calculates speed and speed-up (minimum, maximum and average)

--- Assignment-01/src/main/java/pcd/ass01/Benchmark.java
+++ Assignment-01/src/main/java/pcd/ass01/Benchmark.java
@@ -1,6 +1,8 @@
 package pcd.ass01;
 
 import ch.qos.logback.classic.Level;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pcd.ass01.domain.Board;
@@ -10,36 +12,103 @@ import pcd.ass01.util.LoggingUtils;
 import pcd.ass01.util.time.Stopwatch;
 
 import java.lang.invoke.MethodHandles;
+import java.util.*;
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import static java.util.Map.Entry;
 
 final class Benchmark {
 
     private static final Logger logger;
 
-    private static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
+    public static final int MAX_ITERATIONS = 25;
+    private static final int MAX_NUMBER_OF_WORKERS = 10;
 
-    private static final int STEP = 1_000;
-    private static final int MAX_SIZE = 10_000;
+    private static final int SIZE = 5_000;
 
     public static void main(final String... args) {
         final Stopwatch stopwatch = Stopwatch.stopwatch(TimeUnit.MILLISECONDS);
         final BoardUpdater sequentialUpdater = BoardUpdater.create();
 
+        final Multimap<Integer, Long> results = Multimaps.newListMultimap(new HashMap<>(), ArrayList::new);
+
         sequentialUpdater.start();
-        for (int numberOfWorkers = 1; numberOfWorkers <= AVAILABLE_PROCESSORS + 1; numberOfWorkers++) {
-            final BoardUpdater updater = BoardUpdater.create(numberOfWorkers);
-            updater.start();
-            for (int size = STEP; size <= MAX_SIZE; size += STEP) {
-                final Board board = Boards.gosperGliderGun(size, size);
+        for (int i = 1; i <= MAX_ITERATIONS; i++) {
+            System.out.println("Iteration #" + i);
+            for (int numberOfWorkers = 1; numberOfWorkers <= MAX_NUMBER_OF_WORKERS; numberOfWorkers++) {
+                final BoardUpdater updater = BoardUpdater.create(numberOfWorkers);
+                updater.start();
+                final Board board = Boards.gosperGliderGun(SIZE, SIZE);
 
                 final long updateTime = timeIt(stopwatch, () -> updater.update(board));
 
-                logger.info("{} {}x{} updated with {} worker{} in {} ms", board.getClass().getSimpleName(), size, size, numberOfWorkers, numberOfWorkers > 1 ? "s" : "", updateTime);
+                // logger.info("{} {}x{} updated with {} worker{} in {} ms", board.getClass().getSimpleName(), size, size, numberOfWorkers, numberOfWorkers > 1 ? "s" : "", updateTime);
                 // checkState(Objects.equals(newBoard, sequentialUpdater.update(board)), "Updates are not equal");
+                results.put(numberOfWorkers, updateTime);
+                updater.stop();
             }
-            updater.stop();
         }
         sequentialUpdater.stop();
+
+        final Map<Integer, Long> minSpeeds = results.asMap().entrySet().stream()
+                .map(e -> new SimpleImmutableEntry<>(
+                        e.getKey(),
+                        getSpeeds(e)
+                                .min()
+                                .getAsLong()))
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+
+        final Map<Integer, Long> maxSpeeds = results.asMap().entrySet().stream()
+                .map(e -> new SimpleImmutableEntry<>(
+                        e.getKey(),
+                        getSpeeds(e)
+                                .max()
+                                .getAsLong()))
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+
+        final Map<Integer, Double> avgSpeeds = results.asMap().entrySet().stream()
+                .map(e -> new SimpleImmutableEntry<>(
+                        e.getKey(),
+                        getSpeeds(e)
+                                .average()
+                                .getAsDouble()))
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+
+        System.out.println("Min speeds: " + minSpeeds + " ms");
+        System.out.println("Max speeds: " + maxSpeeds + " ms");
+        System.out.println("Average speeds: " + avgSpeeds + " ns");
+        System.out.println();
+
+        final double singleThreadedMinSpeed = minSpeeds.get(1);
+        minSpeeds.remove(1);
+
+        final double singleThreadedMaxSpeed = maxSpeeds.get(1);
+        maxSpeeds.remove(1);
+
+        final double singleThreadedAvgSpeed = avgSpeeds.get(1);
+        avgSpeeds.remove(1);
+
+        final Map<Integer, Double> minSpeedUps = calculateSpeedUps(minSpeeds, singleThreadedMinSpeed);
+        final Map<Integer, Double> maxSpeedUps = calculateSpeedUps(maxSpeeds, singleThreadedMaxSpeed);
+        final Map<Integer, Double> avgSpeedUps = calculateSpeedUps(avgSpeeds, singleThreadedAvgSpeed);
+
+        System.out.println("Min speed-ups: " + minSpeedUps);
+        System.out.println("Max speed-ups: " + maxSpeedUps);
+        System.out.println("Avg speed-ups: " + avgSpeedUps);
+    }
+
+    private static Map<Integer, Double> calculateSpeedUps(final Map<? extends Integer, ? extends Number> speeds, final double singleThreadedSpeed) {
+        return speeds.entrySet().stream()
+                .map(e -> new SimpleImmutableEntry<>(e.getKey(), e.getValue().doubleValue() / singleThreadedSpeed))
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+    }
+
+    private static LongStream getSpeeds(final Entry<? extends Integer, ? extends Collection<? extends Long>> e) {
+        return e.getValue().stream()
+                .mapToLong(Number::longValue);
     }
 
     private static long timeIt(final Stopwatch stopwatch, final Runnable action) {
@@ -49,7 +118,7 @@ final class Benchmark {
     }
 
     static {
-        LoggingUtils.setLevel(Level.INFO);
+        LoggingUtils.setLevel(Level.ERROR);
         logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     }
 
