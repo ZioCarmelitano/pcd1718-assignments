f9c46b3f6b5f8fefebf2bb3be2deb72a985fda02
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-03-28 23:34:24 +0200

    Gradle finally builds fine. TODO: test scripts on windows

--- Assignment-01/run.bat
+++ Assignment-01/run.bat
@@ -1,5 +1,8 @@
 @echo off
 
+set BENCHMARK_FLAG="-b"
+set GUI_FLAG="-g"
+
 set argc=0
 FOR %%x in (%*) DO (
     set /A argc+=1
@@ -7,7 +10,15 @@ FOR %%x in (%*) DO (
 )
 
 IF /I "%argc%" NEQ "1" (
-    echo "Exacly one argument required, the name of the main class" >&2
+    echo "Usage: %0 <-b|-g>" >&2
+    echo "   %0 %BENCHMARK_FLAG% run benchmark" >&2
+    echo "   %0 %GUI_FLAG% run GUI" >&2
+) ELSE IF /I "%1" == %BENCHMARK_FLAG% (
+    gradlew runBenchmark
+) ELSE IF /I "%1" == %GUI_FLAG% (
+    gradlew runGui
 ) ELSE (
-    gradlew exec -PmainClass=%1
+    @echo "Usage: %0 <-b|-g>" >&2
+    @echo "\t%0 %BENCHMARK_FLAG% run benchmark" >&2
+    @echo "\t%0 %GUI_FLAG% run GUI" >&2
 )
