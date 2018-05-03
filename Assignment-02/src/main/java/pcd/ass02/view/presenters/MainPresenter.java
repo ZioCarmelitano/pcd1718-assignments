65d4032239cfe82153729388a2d1d67c25848b04
Piscaglia Nicola <nicola.piscaglia2@studio.unibo.it>
2018-05-03 16:42:48 +0200

    applied minimal changes to Main Presenter

--- Assignment-02/src/main/java/pcd/ass02/view/presenters/MainPresenter.java
+++ Assignment-02/src/main/java/pcd/ass02/view/presenters/MainPresenter.java
@@ -1,5 +1,6 @@
 package pcd.ass02.view.presenters;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
@@ -11,10 +12,18 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.DirectoryChooser;
+import org.reactivestreams.Subscription;
+import pcd.ass02.domain.Folder;
+import pcd.ass02.domain.SearchStatistics;
+import pcd.ass02.ex3.RxJavaOccurrencesCounter;
+import pcd.ass02.ex3.SearchResultSubscriber;
+import pcd.ass02.interactors.OccurrencesCounter;
 import pcd.ass02.view.datamodel.DocumentResult;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class MainPresenter implements Initializable {
@@ -63,14 +72,61 @@ public class MainPresenter implements Initializable {
         new Thread(new Task<Void>() {
             @Override
             protected Void call(){
-                // TODO call the interactor...
-                //tableItems.add(new DocumentResult("bau.txt", 50));
-                //tableItems.add(new DocumentResult("asd.txt", 50));
-                //tableItems.add(new DocumentResult("ggg.txt", 50));
-
+                performSearch(rootFolder, regularExp, maxDepth);
                 return null;
             }
         }).start();
+    }
+
+    private void performSearch(String path, String regularExp, Integer maxDepth) {
+        final OccurrencesCounter counter = new RxJavaOccurrencesCounter(new SearchResultSubscriber() {
+            private long startTime;
+            private long filesWithOccurrencesCount;
+
+            @Override
+            protected void onNext(SearchStatistics statistics) {
+                final List<String> documentNames = statistics.getDocumentNames();
+                final double averageMatches = statistics.getAverageMatches();
+                final double matchingRate = statistics.getMatchingRate();
+
+                if (documentNames.size() > filesWithOccurrencesCount) {
+                    filesWithOccurrencesCount = documentNames.size();
+                    updateTable(documentNames);
+                    updateStatisticsField(averageMatches, matchingRate);
+                }
+            }
+
+            @Override
+            protected void onComplete(long totalOccurrences) {
+                final long endTime = System.currentTimeMillis();
+                System.out.println();
+                System.out.println("Total occurrences: " + totalOccurrences);
+                System.out.println("Execution time: " + (endTime - startTime) + "ms");
+            }
+
+            @Override
+            public void onSubscribe(Subscription s) {
+                s.request(Long.MAX_VALUE);
+                startTime = System.currentTimeMillis();
+            }
+        });
+
+        final Folder rootFolder = Folder.fromDirectory(new File(path), maxDepth);
+        counter.start();
+        counter.countOccurrences(rootFolder, regularExp);
+        counter.stop();
+    }
+
+    private void updateStatisticsField(double averageMatches, double matchingRate) {
+        Platform.runLater(() -> {
+            txtAverageMatching.setText(String.valueOf(averageMatches));
+            txtMatchingRate.setText(String.valueOf(matchingRate));
+        });
+    }
+
+    //TODO
+    private void updateTable(List<String> documentNames) {
+        Platform.runLater(() -> tableItems.add(new DocumentResult("file.txt", 0)));
     }
 
     @FXML
