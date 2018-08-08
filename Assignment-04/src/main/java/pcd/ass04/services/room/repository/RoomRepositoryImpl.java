84fe644d762a38059c22d8046a4ec8d002e9cd00
mvandi <mattia.vandi@hotmail.it>
2018-08-08 15:42:21 +0200

    Implemented RoomService (#50)
    * Removed while loop in benchmark launcher and refactored class a little bit
    
    * Saved local changes
    
    * Fixed text alignment issues
    
    * Temporary commit
    
    * Added a sketch of the 2nd exercise
    
    * Removed IDEA specific files
    
    * Added exercise 1
    
    * Fixed error in .travis.yml
    
    * Commit before merge
    
    * Implemented exercise 2 skeleton, missing ordering algorithm
    
    * Refactoring exercise 2 and moved exercise 1 GUI files in separate folder
    
    * Added commands feature
    
    * Removed my part of code
    
    * Done a little bit of refactoring
    
    * Refactored a little bit exercise 1
    
    * Removed Msg suffix from exercise 1 messages
    
    * Modified room (and user accordingly) so that when a user sends a message does not receive the message to it self
    
    * Started report using report of Assignment-01 as template
    
    * Added spaces at the end of files
    
    * Added speed-up table, execution times graph and a little bit of refactoring in exercise 1
    
    * Fixed error in causal message ordering
    
    * Implemented causal and total message ordering
    
    * Removed simulated latencies
    
    * Fixed error when user leaves the room
    
    * Finished report
    
    * Implemented service discovery with circuit breaker
    
    * Added deployer and launchers for each service
    
    * Added routes to services
    
    * Removed broker service (it was useless)
    
    * Implemented all routes except '/messages'
    
    * Implemented '/messages' route

---
+++ Assignment-04/src/main/java/pcd/ass04/services/room/repository/RoomRepositoryImpl.java
@@ -0,0 +1,108 @@
+package pcd.ass04.services.room.repository;
+
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import pcd.ass04.services.room.domain.Room;
+import pcd.ass04.services.room.domain.User;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Predicate;
+
+public class RoomRepositoryImpl implements RoomRepository {
+
+    private final AtomicLong counter = new AtomicLong(1);
+    private final Map<Room, List<User>> roomMap;
+
+    public RoomRepositoryImpl() {
+        roomMap = new HashMap<>();
+    }
+
+    @Override
+    public Observable<Room> findAll() {
+        return Observable.fromIterable(roomMap.keySet());
+    }
+
+    @Override
+    public Single<Room> findById(Long id) {
+        return Single.fromCallable(() -> {
+            final Optional<Room> room = roomMap.keySet().stream()
+                    .filter(Predicate.isEqual(id))
+                    .findFirst();
+            if (room.isPresent()) {
+                return room.get();
+            } else {
+                throw new RuntimeException("Could not find room with ID " + id);
+            }
+        });
+    }
+
+    @Override
+    public Single<Long> save(Room room) {
+        return Single.fromCallable(() -> {
+            if (room.getId() != null) {
+                final Room r = findById(room.getId()).blockingGet();
+                r.setName(room.getName());
+            } else {
+                room.setId(counter.getAndIncrement());
+                roomMap.put(room, new ArrayList<>());
+            }
+            return room.getId();
+        });
+    }
+
+    @Override
+    public Single<Long> deleteById(Long id) {
+        return Single.fromCallable(() -> {
+            final Room room = findById(id).blockingGet();
+            roomMap.remove(room);
+            return room.getId();
+        });
+    }
+
+    @Override
+    public Completable addUser(Room room, User user) {
+        return Completable.fromAction(() -> {
+            final List<User> users = roomMap.get(room);
+            if (users == null) {
+                throw new IllegalArgumentException("Room '" + room.getName() + "' does not exist!");
+            }
+
+            users.add(user);
+        });
+    }
+
+    @Override
+    public Single<User> findUserById(Room room, long id) {
+        return Single.fromCallable(() -> {
+            final List<User> users = roomMap.get(room);
+            if (users == null) {
+                throw new IllegalArgumentException("Room '" + room.getName() + "' does not exist!");
+            }
+
+            Optional<User> user = users.stream()
+                    .filter(Predicate.isEqual(id))
+                    .findFirst();
+
+            if (user.isPresent()) {
+                return user.get();
+            } else {
+                throw new RuntimeException("Could not find user with ID " + id);
+            }
+        });
+    }
+
+    @Override
+    public Completable removeUser(Room room, User user) {
+        return Completable.fromAction(() -> {
+            final List<User> users = roomMap.get(room);
+            if (users == null) {
+                throw new IllegalArgumentException("Room '" + room.getName() + "' does not exist!");
+            }
+
+            users.add(user);
+        });
+    }
+
+}
