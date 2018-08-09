72bcc8aeb3affb434cac7fb5d179a5f5ab4c8bfb
Nik <nicola.piscaglia2@studio.unibo.it>
2018-08-09 10:47:26 +0200

    continued impl

--- Assignment-04/ChatClient/src/app/app.module.ts
+++ Assignment-04/ChatClient/src/app/app.module.ts
@@ -2,23 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MessagesComponent } from './messages/messages.component';
 import { SendFieldComponent } from './send-field/send-field.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent,
     LoginComponent,
     SidebarComponent,
     MessagesComponent,
-    SendFieldComponent
+    SendFieldComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
